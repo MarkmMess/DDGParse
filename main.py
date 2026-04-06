8b51af3458226445647a928141a2417b14576fce
MarkmMess <mark.markovsky1@gmail.com>
2026-04-06 14:34:48 +0300

    optimize pagination search

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 
-from selenium.webdriver.support.wait import WebDriverWait
-
-
-def get_info(driver, url_to_find) -> str | None:
+def get_info(driver, url_to_find, position=0) -> tuple[str | None, int]:
     ret = None
     element = driver.find_elements(By.CLASS_NAME, "pAgARfGNTRe_uaK72TAD")
 
-    position = 0
-    for el in element:
+    for el in element[position:]:
         position += 1
-
         link = el.find_element(By.TAG_NAME, "span")
 
         if link.text.startswith(f"https://{url_to_find}"):
-            ret = f"{url_to_find} is found at {position} position"
-            return ret
-    return ret
+            found = link.text
+            return f"{found} is found at {position} position", position
+
+    return ret, position
 
 
 def find_site_position(request, url_to_find):
     # You can switch the browser to Chrome, Edge, Firefox, Ie or Safari
     driver = webdriver.Chrome()
-    ret = None
+    position = 0
+    result = None
     url = f"https://duckduckgo.com/?ia=web&origin=funnel_home_google&t=h_&q={request}&chip-select=search"
     driver.get(url)
     WebDriverWait(driver, 10).until(
@@ -38,21 +36,19 @@ def find_site_position(request, url_to_find):
             button.click()
             time.sleep(2)
 
-        ret = get_info(driver, url_to_find)
+        result, position = get_info(driver, url_to_find, position)
 
-        if ret:
-            ret = ret + f", from page {number}"
+        if result is not None:
+            result = result + f", from page {number}"
             break
 
     driver.quit()
 
-    return ret
+    return result
 
 
-req = input("enter your request: ")
-find_url = input("""enter url to find it's position: """)
+req = (input("enter your request: ")).strip()
+find_url = (input("""enter url to find it's position: """)).strip()
 
-req.strip()
-find_url.strip()
 
 print(find_site_position(req, find_url))
